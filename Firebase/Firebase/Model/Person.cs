a59b7312345e8d98ac4db09d7cfaa979f5fded45
INDUNIL LAKSHITHA <skgindunil@gmail.com>
2019-11-07 15:16:38 +0530

    first up

---
+++ Firebase/Firebase/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firebase.Model
+{
+      public class Person
+        {
+            public int PersonId { get; set; }
+            public string Name { get; set; }
+        }
+    
+}
